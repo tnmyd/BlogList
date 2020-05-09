13087bf33450923bce60a8bf4d6178ac956dc15f
Tanmoy Das <tanmoydas1997@gmail.com>
2020-05-09 21:46:00 +0530

    Moved model to blog.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,11 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
