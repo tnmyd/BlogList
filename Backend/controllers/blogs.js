48351dcb98495e70f8a20d29ab47cb0cac8ceadf
Tanmoy Das <tanmoydas1997@gmail.com>
2020-05-10 12:28:19 +0530

    ES Lint fixes

--- Backend/controllers/blogs.js
+++ Backend/controllers/blogs.js
@@ -2,20 +2,20 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-blogsRouter.post('/', (request, response) => {
-const blog = new Blog(request.body)
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
 
-blog
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
     .save()
     .then(result => {
-    response.status(201).json(result)
+      response.status(201).json(result)
     })
 })
 
--- Backend/index.js
+++ Backend/index.js
@@ -7,5 +7,5 @@ const server = http.createServer(app)
 
 
 server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+	logger.info(`Server running on port ${config.PORT}`)
 })
