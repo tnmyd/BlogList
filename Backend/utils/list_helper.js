1c20774c6c2d4dc9a5a08ac72a4c5ea2a2c13a64
Tanmoy Das <tanmoydas1997@gmail.com>
2020-09-30 13:47:05 +0530

    Completed Exercise 4.4

--- Backend/utils/list_helper.js
+++ Backend/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1;
   }
   
-  module.exports = {
-    dummy
+
+const totalLikes = (blogs) => {
+    const totalLikesReducer = (accumulator, initialValue) => accumulator + initialValue
+    if(blogs.length === 0)
+        return 0
+    return blogs.map(blog => blog.likes).reduce(totalLikesReducer)
+
+}
+
+module.exports = {
+    dummy,
+    totalLikes
   }
