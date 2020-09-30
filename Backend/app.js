9ef787f06609fed78a81a85b74c78628a5a07f02
Tanmoy Das <tanmoydas1997@gmail.com>
2020-09-30 13:08:45 +0530

    Added Model

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
