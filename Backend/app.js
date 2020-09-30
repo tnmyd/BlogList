82c565d4e5f347c55c8af716901e9c481b89d74b
Tanmoy Das <tanmoydas1997@gmail.com>
2020-09-30 13:12:59 +0530

    Added Controller

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 
 const mongoUrl = config.MONGODB_URI;
@@ -11,23 +12,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs',blogsRouter)
 
 module.exports = app;
