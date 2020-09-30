c9813aeff3987450a711a81c4f54822d7002a05d
Tanmoy Das <tanmoydas1997@gmail.com>
2020-09-30 22:52:01 +0530

    Completed Exercise 4.8

--- Backend/utils/config.js
+++ Backend/utils/config.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
-console.log("config",PORT)
+let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV === 'test') { 
+  MONGODB_URI = process.env.TEST_MONGODB_URI 
+}
+
+console.log("config", PORT)
 
 module.exports = {
   MONGODB_URI,
