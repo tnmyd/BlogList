1178fe7a07943fa9e29b134372eefeadd62932e1
Tanmoy Das <tanmoydas1997@gmail.com>
2020-10-07 23:07:38 +0530

    Completed Exercise 5.6 and refactored Login

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import AddBlog from './components/AddBlog'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
-import loginService from './services/login'
 import './App.css'
 import Togglable from './components/Togglable'
-import { useRef } from 'react'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  
   const [errorMessage, setErrorMessage] = useState('');
   const [message, setMessage] = useState('');
 
@@ -31,96 +29,20 @@ const App = () => {
       const user = JSON.parse(loggedInUser);
       setUser(user);
       blogService.setToken(user.token)
-
     }
-
   }, [])
 
   const hideAddBlog = () => {
     addBlogRef.current.toggleVisibility();
   }
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    try {
-      const user = await loginService.login({
-        username,
-        password
-      })
-
-      window.localStorage.setItem(
-        'loggedInUser', JSON.stringify(user)
-      )
-      setUser(user);
-      setPassword('');
-      setUsername('');
-      blogService.setToken(user.token);
-    }
-    catch (exception) {
-      setErrorMessage('Invalid Credentials')
-      setTimeout(() => {
-        setErrorMessage('')
-      }, 5000)
-    }
-  }
-
-  const handleLogout = async (event) => {
-    event.preventDefault();
-
-    window.localStorage.removeItem('loggedInUser')
-    setUser(null)
-  }
-
-  const showUser = () => (
-    <>
-      <p>
-        {user.name} logged in
-    </p>
-      <button onClick={handleLogout}>
-        Logout
-    </button>
-    </>
-  )
-
-  const loginForm = () => (
-    <>
-      <h2>Log in to Application</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-                <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={(event) => setUsername(event.target.value)}
-          />
-
-        </div>
-        <div>
-          password
-                <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </>
-  )
 
   return (
     <div>
       {errorMessage && <Notification message={errorMessage} type="failure" />}
       {message && <Notification message={message} type="success" />}
-
-      {user === null
-        ? loginForm()
-        : showUser()
-      }
+      
+      <LoginForm user={user} setUser={setUser} setErrorMessage={setErrorMessage}/>
       {user
 
         &&
---
+++ Frontend/src/components/LoginForm.js
@@ -0,0 +1,93 @@
+import React,{useState} from 'react';
+import loginService from '../services/login';
+import blogService from '../services/blogs';
+
+const LoginForm = ({user, setUser, setErrorMessage}) => {
+    const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      })
+
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
+      setUser(user);
+      setPassword('');
+      setUsername('');
+      blogService.setToken(user.token);
+    }
+    catch (exception) {
+      setErrorMessage('Invalid Credentials')
+      setTimeout(() => {
+        setErrorMessage('')
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault();
+
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+  }
+
+  const showUser = () => (
+    <>
+      <p>
+        {user.name} logged in
+    </p>
+      <button onClick={handleLogout}>
+        Logout
+    </button>
+    </>
+  )
+
+  const loginForm = () => (
+    <>
+      <h2>Log in to Application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+                <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+
+        </div>
+        <div>
+          password
+                <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  )
+
+    return (
+        <>
+        {user === null
+        ? loginForm()
+        : showUser()
+      }
+        </>
+
+    )
+}
+
+export default LoginForm;
