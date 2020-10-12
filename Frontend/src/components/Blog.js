22ac4189de7597a485b7702cc9de942f34b129b7
Tanmoy Das <tanmoydas1997@gmail.com>
2020-10-12 22:50:01 +0530

    Completed Exercise 5.7

--- Frontend/src/components/Blog.js
+++ Frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+import blogs from '../services/blogs';
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+
+  const details = () => (
+    <div>
+      <h2>{blog.title}<button onClick={()=> setDetailsVisible(!detailsVisible)}>hide</button></h2>
+      <a href={blog.url}>{blog.url}</a>
+      <p>likes <button>like</button></p>
+      <p>{blog.author}</p>
+    </div>
+  )
+
+  const showDetails = (event) => {
+
+  }
+
+
+  return (
+    <div>
+      {
+        detailsVisible
+          ? details()
+          : <>
+          {blog.title}
+          {blog.author}
+          <button onClick={()=> setDetailsVisible(!detailsVisible)}>view</button>
+        </>
+   }
+
+    </div>
+  )
+}
 
 export default Blog
