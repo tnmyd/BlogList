75651984c00d2b3c4a852ab7ec766d8ea10ac6c4
Tanmoy Das <tanmoydas1997@gmail.com>
2020-10-12 23:49:07 +0530

    Completed Exercise 5.9

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,9 +18,11 @@ const App = () => {
 
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    blogService.getAll().then(blogs => {
+      const sortedBlogs = blogs.sort((a,b) => b.likes - a.likes)
+
+      setBlogs(sortedBlogs)
+    })
   }, [blogs])
 
   useEffect(() => {
